b841f35b3ed5a106b512711c3e479d1c131d7b15
Celus8 <celus217@gmail.com>
2022-05-24 16:48:15 +0200

    Cap amount of divs created

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let canvas = document.querySelector("#canvas");
 
 function createGrid(dim) {
     
+    if (dim > 100) {
+        alert("The dimension cannot exceed 100 units!");
+        return;
+    }
+
     canvas.innerHTML = "";
     
     for (let i = 0; i < dim; i++) {
@@ -14,7 +19,7 @@ function createGrid(dim) {
 
     let gridDivs = document.querySelectorAll("#canvas div div")
 
-    let size = 640 / dim;
+    let size = 640 / dim; // 640 = 40 * 16 is the original size
 
     gridDivs.forEach((currentValue) => {
         currentValue.style.cssText = `width: ${size}px; height: ${size}px;`;
